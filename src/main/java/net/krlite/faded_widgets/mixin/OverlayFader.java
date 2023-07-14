c7059daaa7acf1891240adadaa8f139c7264d300
KrLite <68179735+KrLite@users.noreply.github.com>
2023-07-14 17:24:01 +0800

    just cleanup

--- src/main/java/net/krlite/faded_widgets/mixin/OverlayFader.java
+++ src/main/java/net/krlite/faded_widgets/mixin/OverlayFader.java
@@ -1,22 +1,14 @@
 package net.krlite.faded_widgets.mixin;
 
 import net.krlite.faded_widgets.FadedWidgets;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.toast.ToastManager;
-import net.minecraft.client.util.Window;
 import net.minecraft.client.util.math.MatrixStack;
-import org.joml.Matrix4f;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
