da1438f2a4b08a93e58500aa23e5679e0f74f426
KrLite <68179735+KrLite@users.noreply.github.com>
2023-07-14 17:54:13 +0800

    oh

--- src/main/java/net/krlite/faded_widgets/mixin/OverlayFader.java
+++ src/main/java/net/krlite/faded_widgets/mixin/OverlayFader.java
@@ -1,6 +1,8 @@
 package net.krlite.faded_widgets.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.krlite.faded_widgets.FadedWidgets;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.toast.ToastManager;
@@ -13,6 +15,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
 public class OverlayFader {
+	@Inject(method = "renderHand", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V"))
+	private void tiltHand(MatrixStack matrixStack, Camera camera, float tickDelta, CallbackInfo ci) {
+		matrixStack.translate(0.2 * FadedWidgets.fading(), -0.7 * FadedWidgets.fading(), 0);
+	}
+}
+
+@Mixin(MinecraftClient.class)
+class ToastFader {
 	@Redirect(
 			method = "render",
 			at = @At(
@@ -21,16 +31,15 @@ public class OverlayFader {
 			)
 	)
 	private void tiltToast(ToastManager toastManager, MatrixStack matrixStack) {
-		matrixStack.push();
-		matrixStack.translate(160 * FadedWidgets.fading(), 0, 0);
+		MatrixStack modelViewStack = RenderSystem.getModelViewStack();
+
+		modelViewStack.push();
+		modelViewStack.translate(160 * FadedWidgets.fading(), 0, 0);
+		RenderSystem.applyModelViewMatrix();
 
 		toastManager.draw(matrixStack);
 
-		matrixStack.pop();
-	}
-
-	@Inject(method = "renderHand", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V"))
-	private void tiltHand(MatrixStack matrixStack, Camera camera, float tickDelta, CallbackInfo ci) {
-		matrixStack.translate(0.2 * FadedWidgets.fading(), -0.7 * FadedWidgets.fading(), 0);
+		modelViewStack.pop();
+		RenderSystem.applyModelViewMatrix();
 	}
 }
--- src/main/java/net/krlite/faded_widgets/mixin/WidgetsFader.java
+++ src/main/java/net/krlite/faded_widgets/mixin/WidgetsFader.java
@@ -6,30 +6,28 @@ import net.krlite.faded_widgets.FadedWidgets;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(InGameHud.class)
 public class WidgetsFader {
-	@Inject(
+	@ModifyArgs(
 			method = "render",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/client/MinecraftClient;getLastFrameDuration()F",
-					shift = At.Shift.BEFORE
+					target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderColor(FFFF)V"
+			),
+			slice = @Slice(
+					from = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;getLastFrameDuration()F")
 			)
 	)
-	private void setOpacityPre(MatrixStack matrixStack, float tickDelta, CallbackInfo ci) {
-		RenderSystem.setShaderColor(1, 1, 1, (float) (1 - FadedWidgets.fading()));
-	}
-
-	@Inject(method = "render", at = @At("RETURN"))
-	private void setOpacityPost(MatrixStack matrixStack, float tickDelta, CallbackInfo ci) {
-		RenderSystem.setShaderColor(1, 1, 1, 1);
+	private void setOpacity(Args args) {
+		float r = args.get(0), g = args.get(1), b = args.get(2), a = args.get(3);
+		args.set(0, (float) Theory.lerp(0, r, 1 - FadedWidgets.fading())); // red
+		args.set(1, (float) Theory.lerp(0, g, 1 - FadedWidgets.fading())); // green
+		args.set(2, (float) Theory.lerp(0, b, 1 - FadedWidgets.fading())); // blue
+		args.set(3, (float) Theory.lerp(0, a, 1 - FadedWidgets.fading())); // alpha
 	}
 
 	@ModifyArgs(
@@ -78,30 +76,37 @@ public class WidgetsFader {
 			method = "renderHotbar",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbarItem(Lnet/minecraft/client/util/math/MatrixStack;IIFLnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;I)V",
+					target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbarItem(IIFLnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;I)V",
 					shift = At.Shift.BEFORE
 			)
 	)
 	private void tiltHotbarItemPre(float tickDelta, MatrixStack matrixStack, CallbackInfo ci) {
-		matrixStack.push();
+		MatrixStack modelViewStack = RenderSystem.getModelViewStack();
+
+		modelViewStack.push();
 		double shift = 24 * FadedWidgets.fading();
 
 		if (FadedWidgets.isVerticalityLoaded()) { // Support for Verticality
-			matrixStack.translate(-shift, 0, 0);
+			modelViewStack.translate(-shift, 0, 0);
 		} else {
-			matrixStack.translate(0, shift, 0);
+			modelViewStack.translate(0, shift, 0);
 		}
+
+		RenderSystem.applyModelViewMatrix();
 	}
 
 	@Inject(
 			method = "renderHotbar",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbarItem(Lnet/minecraft/client/util/math/MatrixStack;IIFLnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;I)V",
+					target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbarItem(IIFLnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;I)V",
 					shift = At.Shift.AFTER
 			)
 	)
 	private void tiltHotbarItemPost(float tickDelta, MatrixStack matrixStack, CallbackInfo ci) {
-		matrixStack.pop();
+		MatrixStack modelViewStack = RenderSystem.getModelViewStack();
+
+		modelViewStack.pop();
+		RenderSystem.applyModelViewMatrix();
 	}
 }
