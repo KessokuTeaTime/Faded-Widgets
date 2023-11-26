5c591f9eabaa0a937e9c1173bb1c8b995c7deae9
KrLite <68179735+KrLite@users.noreply.github.com>
2023-11-26 18:17:35 +0800

    2.0.1

--- src/main/java/net/krlite/faded_widgets/mixin/fader/DebugHudFader.java
+++ src/main/java/net/krlite/faded_widgets/mixin/fader/DebugHudFader.java
@@ -4,13 +4,9 @@ import net.krlite.faded_widgets.FadedWidgets;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.DebugHud;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.MetricsData;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DebugHud.class)
@@ -38,19 +34,6 @@ public class DebugHudFader {
 
 	@Inject(method = "drawRightText", at = @At("TAIL"))
 	private void tiltRightTextPost(DrawContext context, CallbackInfo ci) {
-		context.getMatrices().pop();
-	}
-
-	@Inject(method = "drawMetricsData", at = @At("HEAD"))
-	private void tiltMetricsDataText(DrawContext context, MetricsData metricsData, int x, int width, boolean showFps, CallbackInfo ci) {
-		double shift = MinecraftClient.getInstance().getWindow().getScaledWidth();
-
-		context.getMatrices().push();
-		context.getMatrices().translate(shift * FadedWidgets.fading(), 0, 0);
-	}
-
-	@Inject(method = "drawMetricsData", at = @At("TAIL"))
-	private void tiltMetricsDataPost(DrawContext context, MetricsData metricsData, int x, int width, boolean showFps, CallbackInfo ci) {
 		context.getMatrices().pop();
 	}
 }
